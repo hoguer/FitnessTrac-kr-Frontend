e929fbb1ea3fdb2cba9b8c2bb7a16245ee383537
Rachel Smith <rachel.smith@fullstackacademy.com>
2022-01-18 20:20:26 -0700

    initial cleanup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Routes, Route, Link, NavLink, BrowserRouter as Router, useNavigate, useParams } from 'react-router-dom';
-import Main from './components/Main';
-import Home from './components/Home';
-import Routines from './components/Routines';
-import Activities from './components/Activities';
-import MyRoutines from './components/MyRoutines';
-import AccountForm from './components/AccountForm'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components';
+import Routines from './components';
+import Activities from './components';
 
 const App = () => {
     return <div>
         <Routes>
             <Route path='/' exact element={<Home />} />
-            <Route path='/main' element={<Main />}/>
-            <Route path='/accountForm' element={<AccountForm />}/>
             <Route path='/routines' element={<Routines />} />
             <Route path='/activities' element={<Activities />} />
-            <Route path='/myRoutines' element={<MyRoutines />}/> 
         </Routes>
     </div>
 }
