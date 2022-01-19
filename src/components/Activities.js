3b64842058a0eed3b6a6d8d5a06ada9c57ec4295
Rachel Smith <rachel.smith@fullstackacademy.com>
2022-01-18 20:35:59 -0700

    Get activities from the API

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { fetchActivities } from '../util';
 
 const Activities = () => {
 
-    return <div>Welcome to the Activities Page.</div>
-}
+    const [activities, setActivities] = useState([])
 
+    useEffect(async () => {
+        const activities = await fetchActivities();
+        setActivities(activities);
+    }, [])
+
+    return <>
+        <div>Welcome to the Activities Page.</div>
+        { activities.map((activity) => {
+            return <div key={activity.id}>
+                <div>Name: {activity.name}</div>
+                <div>Description: {activity.description}</div>
+            </div>
+        }) }
+    </>
+}
 
 export default Activities;
